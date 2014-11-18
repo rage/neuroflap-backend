1696e9182c3810118b40ca7a04594d4fff33da5c
Kauhsa <kauhsa@kapsi.fi>
2014-11-18 11:19:05 +0200

    Huge optimizations

--- cors.php
+++ cors.php
@@ -1,3 +1,4 @@
+<?php
 /**
  *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
  *  origin.
@@ -8,28 +9,25 @@
  *  - https://developer.mozilla.org/en/HTTP_access_control
  *  - http://www.w3.org/TR/cors/
  *
- *  Stolen from https://stackoverflow.com/a/9866124 \o/
+ *  Stolen from https://stackoverflow.com/a/9866124
+ *
  */
-function cors() {
 
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-        exit(0);
-    }
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
 
-cors();
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
--- save.php
+++ save.php
@@ -3,6 +3,12 @@
 require 'cors.php';
 require 'config.php';
 
+$data = json_decode(file_get_contents('php://input'), true);
+if (empty($data['entries']) || empty($data['studentNumber'])) {
+    header('HTTP/1.1 400 Bad Request');
+    exit('hv');
+}
+
 try {
     $conn = new PDO($DB_DSN, $DB_USERNAME, $DB_PASSWORD);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -10,40 +16,26 @@ try {
     exit('Database connection failed!');
 }
 
-function query($sql, $params) {
-    global $conn;
-    $smnt = $conn->prepare($sql);
-    $smnt->execute($params);
-    return $smnt;
-}
-
-$data = json_decode(file_get_contents('php://input'), true);
-if (!empty($data['entries'], $data['studentNumber'])) {
-    exit('hv');
-}
-
 $conn->beginTransaction();
 
 // insert session
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$student_number = $data['studentNumber'];
-
-$res = query('INSERT INTO session (ipAddress, studentNumber) VALUES (:ipAddress, :studentNumber) RETURNING id',
-    array(':ipAddress' => $_SERVER['REMOTE_ADDR'], ':studentNumber' => $student_number));
-
-$session_id = $res->fetchColumn();
+$stmt = $conn->prepare('INSERT INTO session (ipAddress, studentNumber) VALUES (:ipAddress, :studentNumber) RETURNING id');
+$stmt->execute(array(':ipAddress' => $_SERVER['REMOTE_ADDR'], ':studentNumber' => $data['studentNumber']));
+$session_id = $stmt->fetchColumn();
 
 // insert log entries to session
 
+$stmt = $conn->prepare('INSERT INTO entry (sessionId, time, content) VALUES (:sessionId, to_timestamp(:time), :content)');
+
 foreach ($data['entries'] as $entry) {
-    if (!empty($entry['timestamp'], $entry['content'])) {
+    if (empty($entry['time']) || empty($entry['event'])) {
         $conn->rollback();
+        header('HTTP/1.1 400 Bad Request');
         exit('hv');
     }
 
-    query('INSERT INTO entry (sessionId, time, content) VALUES (:sessionId, to_timestamp(:time), :content)',
-        array(':sessionId' => $session_id, ':time' => $entry['timestamp'], ':content' => $entry['content']));
+    $stmt->execute(array(':sessionId' => $session_id, ':time' => $entry['time'], ':content' => $entry['event']));
 }
 
 $conn->commit();
